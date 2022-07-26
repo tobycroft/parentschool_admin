2cd60201f7c0dfd214921a5e0af6090017af6704
火线兔 <silverhawk@foxmail.com>
2022-07-26 17:16:32 +0800

    TuuzMac

--- application/parentschool/admin/StudyTag.php
+++ application/parentschool/admin/StudyTag.php
@@ -185,8 +185,8 @@ class StudyTag extends Admin
             ->addFormItems([ // 批量添加表单项
                 ['hidden', 'id'],
                 ['number', 'study_id', '课程id'],
-                ['select', 'study_type', '课程类型'],
-                ['number', 'tag_id', '标签id', $tag_name],
+                ['select', 'study_type', '课程类型', \Study\Type::get_type()],
+                ['select', 'tag_id', '标签id', $tag_name],
             ]);
 
         return $data
