080546b33f1ab933a6ffcf6c8aa814f3cc45dcf0
火线兔 <silverhawk@foxmail.com>
2022-07-28 16:47:36 +0800

    TuuzMac

--- application/parentschool/admin/StudyWeekly.php
+++ application/parentschool/admin/StudyWeekly.php
@@ -260,12 +260,14 @@ class StudyWeekly extends Admin
         $info["special_tag"] = join(",", $info["special_tag"]);
         // 使用ZBuilder快速创建表单
 
-        $tag_common = TagModel::where("tag_type", "common")->column("id,name");
-        foreach ($tag_common as $key => $value) {
+        $tag_commons = TagModel::where("tag_type", "common")->column("id,name");
+        $tag_common = [];
+        foreach ($tag_commons as $key => $value) {
             $tag_common[strval($key)] = $value;
         }
-        $tag_special = TagModel::where("tag_type", "special")->column("id,name");
-        foreach ($tag_special as $key => $value) {
+        $tag_specials = TagModel::where("tag_type", "special")->column("id,name");
+        $tag_special = [];
+        foreach ($tag_specials as $key => $value) {
             $tag_special[strval($key)] = $value;
         }
         $tag_choose = StudyTagModel::where("study_id", $id)->column("tag_id");
