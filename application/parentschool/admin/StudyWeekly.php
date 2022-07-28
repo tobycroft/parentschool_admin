ce13d1991bab9914433315c0c004fe794a8cd175
火线兔 <silverhawk@foxmail.com>
2022-07-28 15:57:53 +0800

    TuuzMac

--- application/parentschool/admin/StudyWeekly.php
+++ application/parentschool/admin/StudyWeekly.php
@@ -272,8 +272,6 @@ class StudyWeekly extends Admin
         // 使用ZBuilder快速创建表单
 
         $tag_commons = TagModel::where("tag_type", "common")->column("id,name");
-        print_r($tag_commons);
-        exit();
         $tag_common = [];
         foreach ($tag_commons as $tc) {
             $tag_common[$tc["id"]] = $tc["name"];
