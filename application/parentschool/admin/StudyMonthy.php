2d71266d9bb2f724b4e508755cfa3134555f7162
火线兔 <silverhawk@foxmail.com>
2022-07-29 11:43:48 +0800

    TuuzMac

--- application/parentschool/admin/StudyMonthy.php
+++ application/parentschool/admin/StudyMonthy.php
@@ -13,7 +13,6 @@ use app\admin\controller\Admin;
 use app\common\builder\ZBuilder;
 use app\parentschool\model\StudyMonthyModel;
 use app\parentschool\model\StudyTagModel;
-use app\parentschool\model\StudyWeeklyModel;
 use app\parentschool\model\TagModel;
 use app\user\model\Role as RoleModel;
 use app\user\model\User;
@@ -40,7 +39,7 @@ class StudyMonthy extends Admin
         $order = $this->getOrder();
         $map = $this->getMap();
         // 读取用户数据
-        $data_list = StudyWeeklyModel::where($map)->order($order)->paginate()->each(function ($item, $key) {
+        $data_list = StudyMonthyModel::where($map)->order($order)->paginate()->each(function ($item, $key) {
             $item["common_tag"] = StudyTagModel::alias("a")->leftJoin(["ps_tag" => "b"], "a.tag_id=b.id")->where("study_id", $item["id"])->where("b.tag_type", "common")->column("name");
             $item["special_tag"] = StudyTagModel::alias("a")->leftJoin(["ps_tag" => "b"], "a.tag_id=b.id")->where("study_id", $item["id"])->where("b.tag_type", "special")->column("name");
             $item["common_tag"] = join(",", $item["common_tag"]);
