62bd0a5d50bd4c2a9eea483387a1f18acbeb2751
火线兔 <silverhawk@foxmail.com>
2022-08-01 16:23:42 +0800

    TuuzMac

--- application/parentschool/admin/StudyMonthy.php
+++ application/parentschool/admin/StudyMonthy.php
@@ -92,8 +92,8 @@ class StudyMonthy extends Admin
                 ['date', '创建时间'],
             ])
             ->addColumn('right_button', '操作', 'btn')
-            ->addRightButton('按钮', $btn_access)
-            ->addRightButton('新建', $btn_access2)
+            ->addRightButtons([$btn_access, $btn_access2])
+//            ->addRightButton('新建', $btn_access2)
             ->addRightButton('edit') // 添加编辑按钮
             ->addRightButton('delete') //添加删除按钮
             ->setRowList($data_list) // 设置表格数据
