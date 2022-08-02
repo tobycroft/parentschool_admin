4efdd4238826bbe34cfcd6280216158fcf35fa47
火线兔 <silverhawk@foxmail.com>
2022-08-02 19:06:41 +0800

    TuuzMac

--- application/parentschool/admin/StudyDaily.php
+++ application/parentschool/admin/StudyDaily.php
@@ -158,13 +158,13 @@ class StudyDaily extends Admin
         }
 
         $tag_common = TagModel::where("tag_type", "common")->column("id,name");
-        foreach ($tag_common as $key => $value) {
-            $tag_common[strval($key)] = $value;
-        }
+//        foreach ($tag_common as $key => $value) {
+//            $tag_common[strval($key)] = $value;
+//        }
         $tag_special = TagModel::where("tag_type", "special")->column("id,name");
-        foreach ($tag_special as $key => $value) {
-            $tag_special[strval($key)] = $value;
-        }
+//        foreach ($tag_special as $key => $value) {
+//            $tag_special[strval($key)] = $value;
+//        }
 
         // 使用ZBuilder快速创建表单
         return ZBuilder::make('form')
