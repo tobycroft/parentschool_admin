6ca0a7f4a0d11f8f43e67f13161e78b16dcd0039
火线兔 <silverhawk@foxmail.com>
2022-08-02 09:34:43 +0800

    TuuzMac

--- extend/Aoss/Aoss.php
+++ extend/Aoss/Aoss.php
@@ -24,6 +24,10 @@ class Aoss
         $response = curl_exec($ch);
         curl_close($ch);
         $json = json_decode($response, true);
+        if (empty($json) || empty($json["code"])) {
+            return false;
+        }
+
         if ($json["code"] == "0") {
             return $json["data"];
         } else {
