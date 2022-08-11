0ede37ec51861894853b233ccec373acd571ec82
火线兔 <silverhawk@foxmail.com>
2022-08-11 14:55:35 +0800

    TuuzMac

--- application/admin/controller/Attachment.php
+++ application/admin/controller/Attachment.php
@@ -235,7 +235,7 @@ class Attachment extends Admin
                 }
             }
             if (isset($md5_data->error)) {
-                $send_ret = $Aoss->send($info->getPathname(), $info->getMime(), $file_name);
+                $send_ret = $Aoss->send($info->getPathname(), $info->getMime(), $info->getFilename());
                 unlink($info->getPathname());
                 if (isset($send_ret->error)) {
                     return $this->uploadError($from, $send_ret->error, $callback);
