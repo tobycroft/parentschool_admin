791cbff3572b41e739741ebe1b1c7a8cbf4322fe
火线兔 <silverhawk@foxmail.com>
2022-08-11 18:09:37 +0800

    TuuzMac

--- application/parentschool/admin/StudyDaily.php
+++ application/parentschool/admin/StudyDaily.php
@@ -240,9 +240,10 @@ class StudyDaily extends Admin
             if ($md5) {
                 $Aoss = new Aoss(config("upload_prefix"), "complete");
                 $md5_data = $Aoss->md5($md5);
+                var_dump($md5_data);
+                exit();
                 if (empty($md5_data->error)) {
-                    var_dump($md5_data);
-                    exit();
+
                     $data["attach_duration"] = $md5_data->duration;
                 }
             }
