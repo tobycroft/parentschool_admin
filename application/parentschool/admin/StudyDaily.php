df6e4d94ef8a7e1054d882072fa76507d8fc2d75
火线兔 <silverhawk@foxmail.com>
2022-09-14 09:34:28 +0800

    TuuzMac

--- application/parentschool/admin/StudyDaily.php
+++ application/parentschool/admin/StudyDaily.php
@@ -136,9 +136,32 @@ class StudyDaily extends Admin
             $common_tag = $data["common_tag"];
             unset($data["special_tag"]);
             unset($data["common_tag"]);
-            $data2 = $data;
-            unset($data2["end_date"]);
-            if ($user = StudyDailyModel::create($data2)) {
+            $daily_input = [
+                "title" => $data["title"],
+                "slogan" => $data["slogan"],
+                "content" => $data["content"],
+                "img" => $data["img"],
+                "img_intro" => $data["img_intro"],
+                "from1" => $data["from1"],
+                "from2" => $data["from2"],
+                "attach_type" => $data["attach_type"],
+                "attach_url" => $data["attach_url"],
+                "attach_duration" => $data["attach_duration"],
+                "show_to" => $data["show_to"],
+            ];
+            $study_input = [
+                "area_id" => $data["area_id"],
+                "school_id" => $data["school_id"],
+                "grade" => $data["grade"],
+                "push_date" => $data["push_date"],
+                "show_date" => $data["show_date"],
+                "end_date" => $data["end_date"],
+                "can_push" => $data["can_push"],
+                "can_show" => $data["can_show"],
+                "study_type" => $data["study_type"],
+            ];
+            Db::startTrans();
+            if ($user = StudyDailyModel::create($daily_input)) {
                 $lastid = $user->id;
                 if ($special_tag) {
                     foreach ($special_tag as $id) {
@@ -158,24 +181,15 @@ class StudyDaily extends Admin
                         ]);
                     }
                 }
-                StudyModel::create([
-                    "area_id" => $data["area_id"],
-                    "school_id" => $data["school_id"],
-                    "grade" => $data["grade"],
-                    "push_date" => $data["push_date"],
-                    "show_date" => $data["show_date"],
-                    "end_date" => $data["end_date"],
-                    "can_push" => $data["can_push"],
-                    "can_show" => $data["can_show"],
-                    "study_type" => "daily",
-                    "study_id" => $lastid,
-                ]);
-
+                $study_input["study_id"] = $lastid;
+                StudyModel::create($study_input);
+                Db::commit();
                 Hook::listen('user_add', $user);
                 // 记录行为
                 action_log('user_add', 'admin_user', $user['id'], UID);
                 $this->success('新增成功', url('index'));
             } else {
+                Db::rollback();
                 $this->error('新增失败');
             }
         }
