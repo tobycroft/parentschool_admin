6fa2379ce2f6a76620bb32767c424cc887d74d52
火线兔 <silverhawk@foxmail.com>
2022-09-15 11:03:17 +0800

    TuuzMac

--- application/parentschool/admin/StudentOutlet.php
+++ application/parentschool/admin/StudentOutlet.php
@@ -99,7 +99,7 @@ EOF;
             ])
 //            ->setSearch(['school_id' => 'school_id']) // 设置搜索参数
             ->addOrder('id')
-            ->addColumn('school_id', '学校id', "text", "", $school)
+            ->addColumn('school_id', '学校id', "text",)
             ->addColumn('img', '头像', 'img_url')
 //            ->addColumn('grade', '年级',)
 //            ->addColumn('class', '班级',)
