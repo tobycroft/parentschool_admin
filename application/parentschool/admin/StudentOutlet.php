cc5cee69f02e1ce11f068f0dc7abf46ccb8120ca
火线兔 <silverhawk@foxmail.com>
2022-09-15 10:52:56 +0800

    TuuzMac

--- application/parentschool/admin/StudentOutlet.php
+++ application/parentschool/admin/StudentOutlet.php
@@ -102,7 +102,7 @@ EOF;
 //            ->addColumn('class', '班级',)
             ->addColumn('gc', '年级班级',)
             ->addColumn('year', '入学年份',)
-            ->setColumnWidth("img", 300)
+            ->setColumnWidth("img", 400)
             ->setExtraCss($css)
             ->setColumnWidth()
             ->addRightButtons([
