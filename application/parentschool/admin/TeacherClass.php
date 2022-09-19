044abcedf5fa499b51be2c3005424766bd30c185
火线兔 <silverhawk@foxmail.com>
2022-09-19 09:52:27 +0800

    TuuzMac

--- application/parentschool/admin/TeacherClass.php
+++ application/parentschool/admin/TeacherClass.php
@@ -126,8 +126,8 @@ EOF;
                 ['img', '班级二维码', 'img_url'],
                 ['class_id', '第几班', 'text.edit'],
                 ['year', '第几届', 'text.edit'],
-                ['change_date', '修改时间'],
-                ['date', '创建时间'],
+//                ['change_date', '修改时间'],
+//                ['date', '创建时间'],
             ])
             ->addColumn('right_button', '操作', 'btn')
             ->setHeight("auto")
