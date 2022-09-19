2c7a088c0da43627068423fd35a422941645870b
火线兔 <silverhawk@foxmail.com>
2022-09-19 09:26:36 +0800

    TuuzMac

---
+++ application/parentschool/model/SchoolAdminModel.php
@@ -0,0 +1,28 @@
+<?php
+// +----------------------------------------------------------------------
+// | 海豚PHP框架 [ DThinkPHP ]
+// +----------------------------------------------------------------------
+// | 版权所有 2016~2019 广东卓锐软件有限公司 [ http://www.zrthink.com ]
+// +----------------------------------------------------------------------
+// | 官方网站: http://DThinkPHP.com
+// +----------------------------------------------------------------------
+
+namespace app\parentschool\model;
+
+use think\Model;
+
+/**
+ * 后台用户模型
+ * @package app\admin\model
+ */
+class SchoolAdminModel extends Model
+{
+    // 设置当前模型对应的完整数据表名称
+    protected $table = 'ps_school_admin';
+
+    // 设置当前模型对应的完整数据表名称
+
+    // 自动写入时间戳
+    protected $autoWriteTimestamp = true;
+
+}
