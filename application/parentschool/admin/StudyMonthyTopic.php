0fcf7d09d62013ea7140ab966a4d481061589033
火线兔 <silverhawk@foxmail.com>
2022-09-22 15:38:12 +0800

    TuuzMac

--- application/parentschool/admin/StudyMonthyTopic.php
+++ application/parentschool/admin/StudyMonthyTopic.php
@@ -140,7 +140,6 @@ class StudyMonthyTopic extends Admin
                 ['ueditor', 'content', '字内容'],
                 ['select', 'attach_type', '附件类型', '', \Study\Type::get_attach_type()],
                 ['file', 'attach_url', '上传文件'],
-                ['chunk', 'attach_url', '上传文件'],
                 ['text', 'attach_duration', '播放时间'],
             ])
             ->assign([
@@ -216,7 +215,6 @@ class StudyMonthyTopic extends Admin
                 ['ueditor', 'content', '字内容'],
                 ['select', 'attach_type', '附件类型', '', \Study\Type::get_attach_type()],
                 ['file', 'attach_url', '上传文件'],
-                ['chunk', 'attach_url', '上传文件'],
                 ['text', 'attach_duration', '播放时间'],
             ]);
 
