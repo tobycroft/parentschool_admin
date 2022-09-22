19614410e9c84e73a0906ebb528bd607141f067b
火线兔 <silverhawk@foxmail.com>
2022-09-22 16:51:11 +0800

    TuuzMac

--- application/parentschool/admin/StudyDaily.php
+++ application/parentschool/admin/StudyDaily.php
@@ -275,6 +275,8 @@ class StudyDaily extends Admin
                 $md5_data = $Aoss->md5($md5);
                 if (empty($md5_data->error)) {
                     $data["attach_duration"] = $md5_data->duration;
+                } else {
+                    $this->error('远程数据不正确');
                 }
             }
             Db::startTrans();
