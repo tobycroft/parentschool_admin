f5013ab60f432844dd067f011b3bfd76aa4006d0
火线兔 <silverhawk@foxmail.com>
2022-09-23 16:47:21 +0800

    TuuzMac

--- application/parentschool/admin/StudentCompare.php
+++ application/parentschool/admin/StudentCompare.php
@@ -32,8 +32,9 @@ class StudentCompare extends Admin
         $map = $this->getMap();
         // 读取用户数据
 
-        $data_list = StudentModel::field("a.*,b.year as year2,b.class as class2,b.school_id as school_id2")
+        $data_list = StudentModel::field("count(a.id) as count,a.*,b.year as year2,b.class as class2,b.school_id as school_id2")
             ->alias("a")
+            ->group("a.id")
             ->join(["ps_student_outlet" => "b"], "a.name=b.name")
             ->whereNotNull("b.id")
             ->where($map)
