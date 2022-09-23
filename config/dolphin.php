252d0476790afc7091d011f42d414da721fe397f
火线兔 <silverhawk@foxmail.com>
2022-09-23 12:47:22 +0800

    TuuzMac

--- config/dolphin.php
+++ config/dolphin.php
@@ -2,15 +2,15 @@
 
 
 return [
-	// 产品信息
-	'product_name' => 'DThinkPHP',
-	'product_version' => '1.5.0',
-	'build_version' => '202102051729',
-	'product_website' => 'http://www.DThinkPHP.com',
-	'product_update' => 'http://www.DThinkPHP.com/checkUpdate',
-	'develop_team' => 'DThinkPHP',
+    // 产品信息
+    'product_name' => 'DThinkPHP',
+    'product_version' => '1.5.0',
+    'build_version' => '202102051729',
+    'product_website' => 'http://www.DThinkPHP.com',
+    'product_update' => 'http://www.DThinkPHP.com/checkUpdate',
+    'develop_team' => 'DThinkPHP',
 
-	// 公司信息
-	'company_name' => '广东卓锐软件有限公司',
-	'company_website' => 'http://www.zrthink.com',
+    // 公司信息
+    'company_name' => 'ThinkPHP',
+    'company_website' => 'http://www.zrthink.com',
 ];
