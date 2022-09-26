12f418566b3970dbdffeee0c3e9f2439ba077c99
火线兔 <silverhawk@foxmail.com>
2022-09-26 16:40:43 +0800

    TuuzMac

--- application/parentschool/admin/StudentCompare2.php
+++ application/parentschool/admin/StudentCompare2.php
@@ -42,6 +42,7 @@ class StudentCompare2 extends Admin
             ->order($order)
             ->paginate()->each(function ($item) {
                 if (StudentOutletModel::where("year", $item["year"])->where("class", $item["class"])->where("callsign", $item["callsign"])->find()) {
+                    exit();
                     return null;
                 }
                 if ($item["school_id"] != $item["school_id2"]) {
