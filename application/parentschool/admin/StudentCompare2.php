5ca663439ef2252245115f6a4e80b247f13edec4
火线兔 <silverhawk@foxmail.com>
2022-09-26 10:28:48 +0800

    TuuzMac

--- application/parentschool/admin/StudentCompare2.php
+++ application/parentschool/admin/StudentCompare2.php
@@ -29,7 +29,7 @@ class StudentCompare2 extends Admin
     public function index()
     {
         // 获取排序
-        $order = $this->getOrder("name desc");
+        $order = $this->getOrder("grade asc,class asc");
         $map = $this->getMap();
         // 读取用户数据
 
