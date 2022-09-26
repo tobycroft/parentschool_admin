49e5ba2a8ac026881894c5400dd2c66248f662c1
火线兔 <silverhawk@foxmail.com>
2022-09-26 16:50:17 +0800

    TuuzMac

--- application/parentschool/admin/StudentCompare2.php
+++ application/parentschool/admin/StudentCompare2.php
@@ -33,7 +33,7 @@ class StudentCompare2 extends Admin
         $map = $this->getMap();
         // 读取用户数据
 
-        $data_list = StudentModel::field("aa.*,b.year as year2,b.class as class2,b.school_id as school_id2,b.callsign as callsign2,c.wx_name")
+        $data_list = StudentModel::field("a.*,b.year as year2,b.class as class2,b.school_id as school_id2,b.callsign as callsign2,c.wx_name")
             ->alias("a")
             ->join(["ps_student_outlet" => "b"], "a.name=b.name and a.school_id=b.school_id and ( a.year!=b.year or a.class != b.class)")
             ->leftJoin(["ps_user" => "c"], "a.uid=c.id")
