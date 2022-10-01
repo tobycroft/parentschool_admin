114546d840f8a44ce1c8123726d207f48e2d7312
火线兔 <silverhawk@foxmail.com>
2022-10-01 09:03:51 +0800

    TuuzPC

--- application/parentschool/admin/Study.php
+++ application/parentschool/admin/Study.php
@@ -85,8 +85,7 @@ class Study extends Admin
             ->addTopButton("add")
             ->setSearchArea([
                 ['select', 'study_type', '课程类型', "", \Study\Type::get_type()],
-                ['text', 'a.grade', '年级'],
-                ['date_time', 'show_date', '班级'],
+                ['date_time', 'show_date', '展示日期'],
             ])
             ->setPageTitle('列表')
             ->setSearch(['id' => 'ID', "title" => "标题", 'slogan' => 'slogan']) // 设置搜索参数
