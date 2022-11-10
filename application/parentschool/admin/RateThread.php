3899c9618d9c87840608349a5119bdbf39678004
火线兔 <silverhawk@foxmail.com>
2022-11-10 09:07:55 +0800

    Merge remote-tracking branch 'origin/master'

--- application/parentschool/admin/RateThread.php
+++ application/parentschool/admin/RateThread.php
@@ -73,7 +73,7 @@ class RateThread extends Admin
                 ['text', 'year', '入学年份'],
 //                ['text', 'grade', '年级'],
                 ['text', 'class', '班级'],])
-            ->setSearch(['question_id' => '问题ID']) // 设置搜索参数
+            ->setSearch(['a.id' => '问题ID']) // 设置搜索参数
             ->addOrder('a.id')
             ->addColumn('id', 'ID')
             ->addColumn('type', '评价课程类型', "select", \Study\Type::get_type())
