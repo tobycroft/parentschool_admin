0229e4690a26c0929a7e8b7da8f27ea7b78026dd
火线兔 <silverhawk@foxmail.com>
2023-03-21 17:06:39 +0800

    TuuzMac

--- application/lcic/admin/Teacher.php
+++ application/lcic/admin/Teacher.php
@@ -152,11 +152,6 @@ class Teacher extends Admin
 
         // 非超级管理员检查可编辑用户
         if (session('user_auth.role') != 1) {
-            $user_list = LcicModel::where('role', 'in', $role_list)
-                ->column('id');
-            if (!in_array($id, $user_list)) {
-                $this->error('权限不足，没有可操作的用户');
-            }
         }
 
         // 保存数据
