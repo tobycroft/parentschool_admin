46ea09ee86b22d4028a916c9b4bd9aaaf23e6cba
火线兔 <silverhawk@foxmail.com>
2023-04-18 18:02:42 +0800

    TuuzMac

--- application/parentschool/admin/InquireRecord.php
+++ application/parentschool/admin/InquireRecord.php
@@ -73,7 +73,7 @@ class InquireRecord extends Admin
             ->addColumn('correct', '是否正确',)
             ->addColumn('change_date', '修改时间')
             ->addColumn('date', '创建时间')
-            ->addColumn('right_button', '操作', 'btn')
+//            ->addColumn('right_button', '操作', 'btn')
 //            ->addRightButton('edit') // 添加编辑按钮
 //            ->addRightButton('delete') //添加删除按钮
             ->setRowList($data_list) // 设置表格数据
