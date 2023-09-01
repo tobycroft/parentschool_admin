bd43a95f5955fdd2a0101d776c56be27455dac6f
火线兔 <silverhawk@foxmail.com>
2023-09-01 14:16:50 +0800

    TuuzMac

--- application/parentschool/admin/School.php
+++ application/parentschool/admin/School.php
@@ -158,7 +158,7 @@ class School extends Admin
                 $parents = Db::query("SELECT *,count(0) as count FROM `ps_study_record`a left join ps_student b on b.id=a.student_id where school_id=$schoolid and year=$year and class_id=$class_id group by student_id order by count desc limit 3");
                 $datas[$schoolid . "_" . $year . "_" . $class_id] = $parents;
             }
-            echo json($datas);
+            return json_encode($datas, 320);
             exit();
 
         }
