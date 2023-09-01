9d4a08b26ca93952743ff9b83dec6d0db810c346
火线兔 <silverhawk@foxmail.com>
2023-09-01 14:46:01 +0800

    TuuzMac

--- application/parentschool/admin/School.php
+++ application/parentschool/admin/School.php
@@ -6,6 +6,7 @@ namespace app\parentschool\admin;
 use app\admin\controller\Admin;
 use app\common\builder\ZBuilder;
 use app\parentschool\model\AreaModel;
+use app\parentschool\model\BalanceModel;
 use app\parentschool\model\ParentModel;
 use app\parentschool\model\SchoolGradeModel;
 use app\parentschool\model\SchoolModel;
@@ -170,10 +171,10 @@ class School extends Admin
 
                 foreach ($value as $k => $v) {
                     $parent = ParentModel::where("id", $v["uid"])->find();
-//                    $fenshu = BalanceModel::where("uid", $v["uid"])->where("student_id", $v["student_id"])->find();
+                    $fenshu = BalanceModel::where("uid", $v["uid"])->where("student_id", $v["student_id"])->find()->toArray();
                     echo "第" . $int . "名:" . $v["name"] . " 家长姓名:" . $parent["wx_name"] .
-                        "   学习量:" . $v["count"] .
-//                    "   分数:" . empty($fenshu['balance']) ? 0 : round($fenshu['balance']) .
+                    "   学习量:" . $v["count"] .
+                    "   分数:" . empty($fenshu['balance']) ? 0 : round($fenshu['balance']) .
                         "<br>";
                     $int++;
                 }
